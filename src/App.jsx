2ed6ed2a51f6092b603e058193a0096cc2232bb8
Narongchai (lmDOW) <bem255541@gmail.com>
2024-03-21 04:17:25 +0700

    test-deploy

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+import Navbar from './Navbar/Navbar.jsx'
+import Header from './Header/Header.jsx'
+import Project from './Project/Project.jsx'
+
+function App() {
+
+  return (
+    <>
+  
+      <Navbar/>
+      <Header/>
+      <Project/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Project/Project.jsx
@@ -0,0 +1,74 @@
+function Project (){
+    return(
+        <>
+        <section className="">
+           <div className="max-w-screen-xl  mx-auto p-4 justify-center" id="project">
+                <h1 className="text-4xl font-bold text-center mb-5">ผลงานต่าง ๆ</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center ">
+                    <div className="card w-96 bg-base-100 shadow-xl  m-5">
+                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div> 
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                                <div className="card-btn flex  mt-2">
+                                    <button className="btn btn-block   bg-gradient-to-r from-cyan-500 to-blue-500 text-white ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                                    เข้าชม
+                                    </button>
+                                </div>
+                            </div>
+                    </div>
+                    <div className="card w-96 bg-base-100 shadow-xl  m-5">
+                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div> 
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                                <div className="card-btn flex  mt-2">
+                                    <button className="btn btn-block   bg-gradient-to-r from-cyan-500 to-blue-500 text-white ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                                    เข้าชม
+                                    </button>
+                                </div>
+                            </div>
+                    </div>
+                    <div className="card w-96 bg-base-100 shadow-xl  m-5">
+                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                Shoes!
+                                <div className="badge badge-secondary">NEW</div>
+                                </h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div> 
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                                <div className="card-btn flex  mt-2">
+                                    <button className="btn btn-block   bg-gradient-to-r from-cyan-500 to-blue-500 text-white ">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                                    เข้าชม
+                                    </button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+    )
+}
+export default Project
