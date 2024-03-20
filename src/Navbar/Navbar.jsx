1f8ebf45554101926a504045d7d0acedb5d57fed
Narongchai (lmDOW) <bem255541@gmail.com>
2024-03-20 15:03:56 +0700

    add Navbar

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar (){
+    return(
+       <nav className="flex justify-around p-5 shadow">
+            <ul className="flex justify-between">
+              logo
+            </ul>
+            <ul className="flex justify-between ">
+                <a href="" className="px-2">หน้าหลัก</a>
+                <a href="" className="px-2">ประวัติการศึกษา</a>
+                <a href="" className="px-2">ผลงาน</a>
+            </ul>
+            <ul className="flex justify-between">
+               
+            </ul>
+       </nav>
+    )
+}
+export default Navbar
