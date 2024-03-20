e918d570977bf162fb2b9f62762eb9ae1edb8afb
Narongchai (lmDOW) <bem255541@gmail.com>
2024-03-21 04:30:16 +0700

    Update Header.jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header(){
                             <div className="flex mt-5">
                                 <button className=" btn  text-2xl  md:btn-lg  xl:text-3xl lg:text-3xl w-full md:w-1/2   bg-gradient-to-r from-cyan-500 to-blue-500 transition  text-white ">
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
-                                ติดต่อ
+                                ติดต่อเลย
                                 </button>
                             
                             </div>
