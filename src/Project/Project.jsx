f8773ac2c339a7d32a035d5ead16044d68598f94
Narongchai (lmDOW) <bem255541@gmail.com>
2024-08-03 02:46:15 +0700

    Update Project.jsx

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -27,7 +27,7 @@ function Project (){
                                     <div className="badge ">API</div>
                                 </div>
                                 <div className="card-btn flex  mt-2">
-                                    <a href="https://nrcdev751.github.io/E-commerce-Dashboard/" target="_blank" className="btn btn-block  bg-gradient-to-r from-cyan-500 to-blue-500 text-white ">
+                                    <a href="https://nrcdev751.github.io/InfiniteScroll/" target="_blank" className="btn btn-block  bg-gradient-to-r from-cyan-500 to-blue-500 text-white ">
                                     <svg className="h-5 w-5 fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
                                     เข้าชม
                                     </a>
